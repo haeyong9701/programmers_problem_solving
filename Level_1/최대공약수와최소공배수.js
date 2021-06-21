0c75874cb5fedf78044daafa71e7150f4c527853
Yoon Haeyong <haeyong9701@gmail.com>
2021-06-21 21:08:48 +0900

    Fix 최대공약수와최소공배수 for comment

--- Level_1/최대공약수와최소공배수.js
+++ Level_1/최대공약수와최소공배수.js
@@ -6,7 +6,7 @@ function solution(n, m) {
   
   let GCD = 0; // 최대공약수
   let temp = 0; // temp는 임시저장소
-  // large를 small으로 나눠서 나머지가 0이 된다면, 최대공약수는 small이 된다.
+  // GCD를 temp으로 나눠서 나머지가 0이 된다면, 최대공약수는 GCD가 된다.
   // 나머지가 0이 아닐 떄, 최대공약수는 나머지가 0이 될때까지 해야된다. (while문 사용)
   while (largeNum % smallNum > 0) {
       temp = largeNum % smallNum; // 1
